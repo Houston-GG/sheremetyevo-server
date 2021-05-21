bf53ab1e4a490398373dc9cd4053c268b7b13064
Sibirny Nikita <rxstr1ker007@gmail.com>
2021-05-22 01:26:59 +0300

    [FEAT] Added base (technic) entity layer.

---
+++ src/main/java/tech/visdom/sheremetyevo/dao/TechnicDao.java
@@ -0,0 +1,7 @@
+package tech.visdom.sheremetyevo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import tech.visdom.sheremetyevo.model.Technic;
+
+public interface TechnicDao extends CrudRepository<Technic, Long> {
+}
---
+++ src/main/java/tech/visdom/sheremetyevo/model/Technic.java
@@ -0,0 +1,43 @@
+package tech.visdom.sheremetyevo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "technic")
+public class Technic {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name="NAME", nullable = false)
+    private String name;
+
+    @Column(name="TYPE_ID", nullable = false)
+    private Long typeId;
+
+    @Column(name="MODEL_ID", nullable = false)
+    private Long modelId;
+
+    @Column(name="CURRENT_FUEL_RESERVE", nullable = false)
+    private Float currentFuelReserve;
+
+    @Column(name="CURRENT_REAGENT_RESERVE", nullable = false)
+    private Float currentReagentReserve;
+
+    @Column(name="STATUS_ID", nullable = false)
+    private Long statusId;
+
+    @Column(name="LATITUDE", nullable = false)
+    private Float latitude;
+
+    @Column(name="LONGITUDE", nullable = false)
+    private Float longitude;
+
+    @Column(name="LAST_UPDATE", nullable = false)
+    private LocalDateTime lastUpdate;
+}
